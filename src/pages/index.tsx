5e85587d26a0c06991ac21d0a09652507766c115
Alex Crocker <croc@hey.com>
2022-05-12 16:04:02 -0400

    Adds special thanks section, moves nav to top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,21 @@ const IndexPage = () => {
 	return (
 		<Wrapper className="px-4 py-10">
 			<Seo title="Demo" />
+			<nav className="my-4 flex flex-row flex-nowrap justify-start items-center">
+				<a
+					className="underline font-semibold text-primary hover:text-primary-light py-1 px-2"
+					target={`_blank`}
+					href="https://github.com/crock/gatsby-starter-frontmatter-kitchen"
+				>
+					GitHub
+				</a>
+				<Link
+					className="underline font-semibold text-primary hover:text-primary-light py-1 px-2"
+					to="/blog"
+				>
+					Blog
+				</Link>
+			</nav>
 			<h1 className="font-black text-2xl mb-3">{siteTitle}</h1>
 			<div className="font-normal text-base leading-6">
 				<p>
@@ -34,21 +49,14 @@ const IndexPage = () => {
 						<li key={feature}>{feature}</li>
 					))}
 				</ul>
-				<nav className="my-4 flex flex-row flex-nowrap justify-start items-center">
-					<a
-						className="underline font-semibold text-primary hover:text-primary-light py-1 px-2"
-						target={`_blank`}
-						href="https://github.com/crock/gatsby-starter-frontmatter-kitchen"
-					>
-						GitHub
-					</a>
-					<Link
-						className="underline font-semibold text-primary hover:text-primary-light py-1 px-2"
-						to="/blog"
-					>
-						Blog
-					</Link>
-				</nav>
+				<h3 className="font-black font-xl mt-4 mb-2 uppercase">
+					Special Thanks:
+				</h3>
+				<ul className="list-disc list-inside">
+					<li>
+						Thanks to <a href="https://macarthur.me/?ref=gatsby-starter-frontmatter-kitchen" target="_blank">Alex McArhtur</a> for creating <a href="https://jamcomments.com/?ref=gatsby-starter-frontmatter-kitchen" target="_blank">a wonderful, JAMstack alternative</a> to Disqus comments.
+					</li>
+				</ul>
 			</div>
 		</Wrapper>
 	)
