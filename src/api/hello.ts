c110afb91a8aed3d14e9142cbc39f562e6c869ae
Alex Crocker <croc@hey.com>
2022-05-12 14:04:53 -0400

    Disabled createPage calls in gatsby-node

--- src/api/hello.ts
+++
@@ -1,6 +0,0 @@
-import { GatsbyFunctionRequest, GatsbyFunctionResponse } from 'gatsby'
-
-export default async (req: GatsbyFunctionRequest, res: GatsbyFunctionResponse) => {
-    
-    res.status(200).send("Hello World!")
-}
